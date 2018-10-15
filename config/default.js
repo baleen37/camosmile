ce169650a58239f94a276efa2180ecf975e317be
Jiho Lee <baleen37@gmail.com>
2018-10-15 23:58:31 +0900

    Add gitignore files

--- config/default.js
+++ config/default.js
@@ -1,9 +1,11 @@
 module.exports = {
     google_auth: {
+        client_id : "1234567890",
         project_id: "camosmile-218915",
         auth_uri: "https://accounts.google.com/o/oauth2/auth",
         token_uri: "https://www.googleapis.com/oauth2/v3/token",
         auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_secret: "1234567890",
         redirect_uris: ["http://localhost:3000/oauth2callback"]
     },
 };
